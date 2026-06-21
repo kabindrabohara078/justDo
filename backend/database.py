aacbe0f8e876f917b814075c356d82f752ce73e5
Kabindra Bohara <kabindrabohara1@gmail.com>
2026-06-21 09:28:43 +0530

    Configure secure database connection

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 # 🔗 Your Neon connection string
-DATABASE_URL = "postgresql://neondb_owner:npg_iV0sJUxaZB3b@ep-cold-recipe-aiolzd4k.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require"
+# DATABASE_URL = "postgresql://neondb_owner:npg_iV0sJUxaZB3b@ep-cold-recipe-aiolzd4k.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 # Create the database engine (the engine manages the network connection to Neon)
 engine = create_engine(DATABASE_URL)
